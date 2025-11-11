a6d3f1ba878eea86e3e8e4459366099241894281
PalkinEgor <palkin122004@gmail.com>
2025-11-11 19:41:13 +0700

    add train encoder

---
+++ train_encoder.py
@@ -0,0 +1,175 @@
+import json
+import argparse
+import torch
+import pandas as pd
+import torch.nn.functional as F
+from transformers import AutoTokenizer, AutoModel
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+
+
+# Класс кастомной модели
+class Model(torch.nn.Module):
+    def __init__(self, model_name, output_dim, freeze_bert=True):
+        super().__init__()
+
+        # Bert енкодер
+        self.bert = AutoModel.from_pretrained(model_name)
+
+        # Проекционная голова для e вектора
+        self.e_proj = torch.nn.Linear(self.bert.config.hidden_size, output_dim)
+
+        # Проекционная голова для m вектора
+        self.m_proj = torch.nn.Linear(self.bert.config.hidden_size, output_dim)
+
+        # Проекционная голова для среднего значения распределения длин
+        self.mu = torch.nn.Linear(self.bert.config.hidden_size, 1)
+
+        # Проекционная голова для стандартного отклонения распределения длин
+        self.std = torch.nn.Linear(self.bert.config.hidden_size, 1)
+
+        # Заморозка модели
+        if freeze_bert:
+            for param in self.bert.parameters():
+                param.requires_grad = False
+
+    def forward(self, input_ids, attention_mask=None):
+        out = self.bert(input_ids, attention_mask)
+        cls = out.last_hidden_state[:, 0, :]
+        e = self.e_proj(cls)
+        m = self.m_proj(cls)
+        mu = self.mu(cls)
+        std = self.std(cls)
+        return e, m, mu, std        
+
+# Dataset для текстов и соответствующих векторов
+class TextDataset(Dataset):
+    def __init__(self, texts, vectors):
+        self.texts = texts
+        self.vectors = vectors
+    
+    def __len__(self):
+        return len(self.texts)
+    
+    def __getitem__(self, idx):
+        return self.texts[idx], self.vectors[idx]
+
+# Подготовка батча перед подачей в модель
+def collate_fn(batch, tokenizer, decoder_tokenizer, device):
+    texts = [item[0] for item in batch]
+    vectors = [torch.tensor(item[1]) for item in batch]
+    input_ids = [tokenizer(text, add_special_tokens=True, return_tensors='pt')['input_ids'].reshape(-1) for text in texts]
+    input_ids = pad_sequence(input_ids, batch_first=True, padding_value=tokenizer.pad_token_id).to(device)
+    attention_mask = (input_ids != tokenizer.pad_token_id).long().to(device)
+    vectors = torch.stack(vectors).to(device)
+    lengths = torch.tensor([len(decoder_tokenizer(text, return_tensors='pt')['input_ids'].reshape(-1)) for text in texts]).to(device)
+    return {
+        'input_ids': input_ids,
+        'attention_mask': attention_mask,
+        'vectors': vectors,
+        'lengths': lengths
+    }
+
+# Реализация InfoNCE loss
+def info_nce_loss(e_pred, m_pred, e_target, m_target, temperature=0.07):
+    # Нормализация e и m векторов
+    e_pred = F.normalize(e_pred, dim=-1)
+    m_pred = F.normalize(m_pred, dim=-1)
+    e_target = F.normalize(e_target, dim=-1)
+    m_target = F.normalize(m_target, dim=-1)
+
+    # Составление матриц похожести
+    e_sim = torch.matmul(e_pred, e_target.T) / temperature
+    m_sim = torch.matmul(m_pred, m_target.T) / temperature
+
+    # Выбор позитивных пар
+    e_positive = torch.arange(e_pred.size(0), device=e_pred.device)
+    m_positive = torch.arange(m_pred.size(0), device=m_pred.device)
+
+    # Расчет функции потерь
+    e_loss = F.cross_entropy(e_sim, e_positive)
+    m_loss = F.cross_entropy(m_sim, m_positive)
+    loss = 0.5 * (e_loss + m_loss)
+    return loss
+
+# Реализация функции потерь для длин ответов
+def gaussian_loss(target, mu, std):
+    return (0.5 * (std + ((target - mu) ** 2) / torch.exp(std))).mean()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--model_name', type=str, default='FacebookAI/xlm-roberta-base')
+    parser.add_argument('--decoder_model_name', type=str, default='Qwen/Qwen2.5-0.5B')
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--num_epochs', type=int, default=20)
+    parser.add_argument('--seed', type=int, default=42)
+    args = parser.parse_args()
+
+    torch.manual_seed(args.seed)
+
+    DATASET_NAME = 'data/training_results.json'
+    DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    HYPERPARAMS = {
+        'lr': 0.0005,
+        'weight_decay': 0.01,
+        'betas': (0.9, 0.9)
+    }
+
+    with open(DATASET_NAME, 'r', encoding='utf-8') as f:
+        data = pd.DataFrame(json.load(f))
+    OUTPUT_DIM = len(data['best_vectors'].to_list()[0][0])
+    
+    # Загрузка модели и токенизаторов
+    model = Model(args.model_name, OUTPUT_DIM).to(DEVICE)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    decoder_tokenizer = AutoTokenizer.from_pretrained(args.decoder_model_name)
+
+    # Формирование Датасетов и Даталоадеров
+    X = data['instruction'].to_list()
+    y = data['best_vectors'].to_list()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=args.seed)
+    train_dataset = TextDataset(X_train, y_train)
+    test_dataset = TextDataset(X_test, y_test)
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, 
+                                collate_fn=lambda x: collate_fn(x, tokenizer, decoder_tokenizer, DEVICE))
+    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, 
+                                collate_fn=lambda x: collate_fn(x, tokenizer, decoder_tokenizer, DEVICE))
+    
+    optimizer = torch.optim.AdamW(model.parameters(), lr=HYPERPARAMS['lr'], betas=HYPERPARAMS['betas'], weight_decay=HYPERPARAMS['weight_decay'])
+    for i in range(args.num_epochs):
+
+        # Прогон эпохи обучения модели
+        train_loss = 0.0    
+        for batch in train_dataloader:
+            optimizer.zero_grad()
+            
+            input_ids = batch['input_ids']
+            attention_mask = batch['attention_mask']
+            lengths = batch['lengths']
+            e_target = batch['vectors'][:, 0, :]
+            m_target = batch['vectors'][:, 1, :]
+            
+            e_pred, m_pred, mu_pred, std_pred = model(input_ids, attention_mask)
+            loss = info_nce_loss(e_pred, m_pred, e_target, m_target) + gaussian_loss(lengths, mu_pred, std_pred)
+            train_loss += loss.item()
+        
+            loss.backward()
+            optimizer.step()
+
+        # Прогон эпохи оценки модели
+        val_loss = 0.0
+        with torch.no_grad():
+            for batch in test_dataloader:
+                input_ids = batch['input_ids']
+                attention_mask = batch['attention_mask']
+                lengths = batch['lengths']
+                e_target = batch['vectors'][:, 0, :]
+                m_target = batch['vectors'][:, 1, :]
+
+                e_pred, m_pred, mu_pred, std_pred = model(input_ids, attention_mask)
+                loss = info_nce_loss(e_pred, m_pred, e_target, m_target) + gaussian_loss(lengths, mu_pred, std_pred)
+                val_loss += loss.item()
+            
+        print(f'Epoch: {i + 1}; Train Loss: {train_loss}; Eval Loss: {val_loss}')
--- train_two_embeddings_capacity.py
+++ train_two_embeddings_capacity.py
@@ -9,6 +9,7 @@ from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset, DataLoader
 
 
+# Dataset для текстов
 class TextDataset(Dataset):
     def __init__(self, texts):
         self.texts = texts
@@ -19,6 +20,7 @@ class TextDataset(Dataset):
     def __getitem__(self, idx):
         return self.texts[idx], idx
 
+# Оценка классического подхода
 def evaluate_plain_model(model, input_ids):
     model.eval()
     with torch.no_grad():
@@ -34,15 +36,21 @@ def evaluate_plain_model(model, input_ids):
         accuracy, seq_accuracy, correct_prefix_length = calculate_metrics(targets_for_loss, pred)
         return loss, accuracy, seq_accuracy, correct_prefix_length
 
+# Создание схемы с одним e вектором и text_length - 1 m векторов
 def generate_input_one(vectors, text_length):
     return torch.cat([vectors[:1, None, :], vectors[1:2, None, :].expand(-1, text_length - 1, -1)], dim=1)
 
+# Создание схемы для целого батча
 def generate_input(batch_vectors, lengths):
     embeds = []
     for vectors, length in zip(batch_vectors, lengths):
         embeds.append(generate_input_one(vectors, length).squeeze(0))
     return pad_sequence(embeds, batch_first=True, padding_value=0.0)    
 
+# Функция для расчета метрик. 
+# accuracy - точность на уровне токенов; 
+# correct_prefix_length - длина правильного суффикса;
+# seq_accuracy - относительная длина правильного суффикса;
 def calculate_metrics(target, pred):
     accuracy = (pred == target).float().mean().item()
     first_wrong = torch.nonzero((pred != target).float())
@@ -54,6 +62,7 @@ def calculate_metrics(target, pred):
         correct_prefix_length = first_wrong[0].item()
     return accuracy, seq_accuracy, correct_prefix_length
 
+# Подготовка батча перед подачей в модель
 def collate_fn(batch, tokenizer, device):
     texts = [item[0] for item in batch]
     indices = [item[1] for item in batch]
@@ -68,6 +77,7 @@ def collate_fn(batch, tokenizer, device):
         'indices': indices
     }
 
+# Безопасное семплирование из категорий
 def safe_sample(group, n, seed):
     if len(group) < n:
         return group
@@ -97,15 +107,22 @@ if __name__ == '__main__':
     
     dataset = load_dataset(DATASET_NAME)
     df = dataset['train'].to_pandas()
+
+    # Выбираем ответы, которые длиннее min_words слов
     df = df[df['response'].apply(lambda x: len(x.split(' ')) > args.min_words)].reset_index(drop=True)
+
+    # Выбор из каждой категории по sample_size примеров
     df = df.groupby(by='category', group_keys=False).apply(lambda x: safe_sample(x, args.sample_size, args.seed)).reset_index(drop=True)
     texts = list(df['response'])
+
+    # Обрезка слишком длинных ответов до max_words слов
     texts = [' '.join(text.split(' ')[:args.max_words]) if len(text.split(' ')) > args.max_words else text for text in texts]
 
     tokenizer = AutoTokenizer.from_pretrained(args.model_name)
     text_dataset = TextDataset(texts)
     text_dataloader = DataLoader(text_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=lambda x: collate_fn(x, tokenizer, DEVICE))
 
+    # Заморозка модели
     model = AutoModelForCausalLM.from_pretrained(args.model_name).to(DEVICE)
     for param in model.parameters():
         param.requires_grad = False
@@ -120,10 +137,11 @@ if __name__ == '__main__':
         indices = batch['indices']
         labels = tokenized_text.clone()
         
+        # Создание обучаемых векторов e и m
         vectors = torch.nn.Parameter(torch.randn(args.batch_size, 2, model.config.hidden_size, device=DEVICE))
         optimizer = torch.optim.AdamW([vectors], lr=HYPERPARAMS['lr'], betas=HYPERPARAMS['betas'], weight_decay=HYPERPARAMS['weight_decay'])
-
-        # vanilla_loss, vanilla_accuracy, vanilla_seq_accuracy, vanilla_correct_prefix_length = evaluate_plain_model(model, tokenized_text)
+        
+        # Хранение лучших метрик
         max_accuracy = torch.zeros(args.batch_size, device=DEVICE)
         max_seq_accuracy = torch.zeros(args.batch_size, device=DEVICE)
         best_vectors = [None] * args.batch_size
@@ -136,16 +154,23 @@ if __name__ == '__main__':
             logits = model(inputs_embeds=current_input, attention_mask=attention_mask).logits
             logits = logits.reshape(-1, logits.shape[-1])
             loss = torch.nn.functional.cross_entropy(logits, labels.reshape(-1), ignore_index=tokenizer.pad_token_id)
-
             pred = logits.argmax(dim=-1).view(tokenized_text.shape)
+
+            # Обновление метрик
             correct_counter = 0
             for i in range(args.batch_size):
+
+                # При достижении идеальной точности пропускаем пример
                 if max_accuracy[i] == 1.0:
                     correct_counter += 1
                     continue
+
+                # Берем предсказания без паддинга и считаем метрики
                 current_pred = pred[i, :lengths[i]]
                 current_labels = labels[i, :lengths[i]]
                 accuracy, seq_accuracy, correct_prefix_length = calculate_metrics(current_labels, current_pred)
+
+                # Сохранение лучших метрик
                 if (accuracy > max_accuracy[i]) or (accuracy == max_accuracy[i] and seq_accuracy > max_seq_accuracy[i]):
                     max_accuracy[i] = accuracy
                     max_seq_accuracy[i] = seq_accuracy
@@ -157,6 +182,7 @@ if __name__ == '__main__':
             loss.backward()
             optimizer.step()
         
+        # Обновление результатов
         for i in range(args.batch_size):
             result.append({
                 'instruction': df.iloc[indices[i]]['instruction'],
@@ -169,5 +195,6 @@ if __name__ == '__main__':
                 'best_vectors': best_vectors[i].cpu().numpy().tolist()
         })
     
+    # Сохранение результатов
     with open('data/training_results.json', 'w', encoding='utf-8') as f:
         json.dump(result, f, ensure_ascii=False, indent=4)
