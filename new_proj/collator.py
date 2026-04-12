12b8780ed05ce3a37f83b6cd01945a5418e896ab
PalkinEgor <palkin122004@gmail.com>
2026-04-13 03:27:59 +0700

    implement code books model

--- new_proj/collator.py
+++ new_proj/collator.py
@@ -1,7 +1,7 @@
 from torch.nn.utils.rnn import pad_sequence
 
 
-# Для датасета yahma/alpaca-cleaned
+# Для датасета yahma/alpaca-cleaned, для задачи векторизации
 def collate_alpaca(batch, tokenizer, max_tokens):
     texts = [item[0] for item in batch]
     instructions = [item[1] for item in batch]
@@ -22,7 +22,7 @@ def collate_alpaca(batch, tokenizer, max_tokens):
         }
     }
 
-# Для датасета databricks/databricks-dolly-15k
+# Для датасета databricks/databricks-dolly-15k, для задачи векторизации
 def collate_dolly(batch, tokenizer, max_tokens):
     texts = [item[0] for item in batch]
     instructions = [item[1] for item in batch]
@@ -38,6 +38,64 @@ def collate_dolly(batch, tokenizer, max_tokens):
         'attention_mask': attention_mask,
         'lengths': lengths,
         'labels': input_ids.clone(),
+        'metainfo': {
+            'texts': texts,
+            'instructions': instructions,
+            'categories': categories
+        }
+    }
+
+# # Для датасета yahma/alpaca-cleaned, для задачи end2end
+# def collate_alpaca_end2end(batch, tokenizer, max_tokens):
+#     texts = [item[0] for item in batch]
+#     instructions = [item[1] for item in batch]
+    
+#     input_ids = [tokenizer.encode(instruction, return_tensors='pt', max_length=max_tokens, truncation=True).reshape(-1) for instruction in instructions]
+#     lengths = [text.shape[0] for text in input_ids]
+#     input_ids = pad_sequence(input_ids, batch_first=True, padding_value=tokenizer.pad_token_id)
+#     attention_mask = (input_ids != tokenizer.pad_token_id).long()
+
+#     return {
+#         'input_ids': input_ids,
+#         'attention_mask': attention_mask,
+#         'lengths': lengths,
+#         'labels': input_ids.clone(),
+#         'metainfo': {
+#             'texts': texts,
+#             'instructions': instructions
+#         }
+#     }
+
+# Для датасета databricks/databricks-dolly-15k, для задачи end2end
+def collate_dolly_end2end(batch, tokenizer, max_tokens):
+    texts = [item[0] for item in batch]
+    instructions = [item[1] for item in batch]
+    categories = [item[2] for item in batch]
+
+    # Готовим интсрукции
+    ins_input_ids = [tokenizer.encode(instruction, return_tensors='pt', max_length=max_tokens, truncation=True).reshape(-1) for instruction in instructions]
+    ins_lengths = [text.shape[0] for text in ins_input_ids]
+    ins_input_ids = pad_sequence(ins_input_ids, batch_first=True, padding_value=tokenizer.pad_token_id)
+    ins_attention_mask = (ins_input_ids != tokenizer.pad_token_id).long()
+
+    # Готовим ответ
+    ans_input_ids = [tokenizer.encode(text, return_tensors='pt', max_length=max_tokens, truncation=True).reshape(-1) for text in texts]
+    ans_lengths = [text.shape[0] for text in ans_input_ids]
+    ans_input_ids = pad_sequence(ans_input_ids, batch_first=True, padding_value=tokenizer.pad_token_id)
+    ans_attention_mask = (ans_input_ids != tokenizer.pad_token_id).long()
+
+    return {
+        'instruction': {
+            'input_ids': ins_input_ids,
+            'attention_mask': ins_attention_mask,
+            'lengths': ins_lengths
+        },
+        'answer': {
+            'input_ids': ans_input_ids,
+            'attention_mask': ans_attention_mask,
+            'lengths': ans_lengths,
+            'labels': ans_input_ids.clone()
+        },
         'metainfo': {
             'texts': texts,
             'instructions': instructions,
---
+++ new_proj/models.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import AutoModel, AutoModelForCausalLM
+
+
+# Модель с использованием кодовых книг
+class CodeBooksModel(nn.Module):
+    def __init__(self, encoder_name, decoder_name, e_code_books, m_code_books, dtype):
+        super().__init__()
+
+        self.encoder = AutoModel.from_pretrained(encoder_name, torch_dtype=dtype, device_map='auto')
+        self.decoder = AutoModelForCausalLM.from_pretrained(decoder_name, torch_dtype=dtype, device_map='auto')
+        for param in self.decoder.parameters():
+            param.requires_grad = False
+        self.decoder.eval()
+        self.e_classifier = nn.Linear(self.encoder.config.hidden_size, e_code_books.shape[0])
+        self.m_classifier = nn.Linear(self.encoder.config.hidden_size, m_code_books.shape[0])
+        self.e_code_books = torch.nn.Parameter(e_code_books)
+        self.m_code_books = torch.nn.Parameter(m_code_books)
+    
+    def forward(self, input_ids, attention_mask=None):
+        x = self.encoder(input_ids=input_ids, attention_mask=attention_mask).last_hidden_state[:, -1, :]
+
+        # получение вектора e
+        e_logits = self.e_classifier(x)
+        e_probs = F.gumbel_softmax(e_logits, tau=1.0, hard=True, dim=-1)
+        e_code_book = e_probs @ self.e_code_books
+
+        # получение вектора m
+        m_logits = self.m_classifier(x)
+        m_probs = F.gumbel_softmax(m_logits, tau=1.0, hard=True, dim=-1)
+        m_code_book = m_probs @ self.m_code_books
+        
+        return e_code_book, m_code_book
